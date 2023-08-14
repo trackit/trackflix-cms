904cbe245fce11c8523912a6353de6c49fd449a8
mezz <mohamed.mazouz@epitech.eu>
2023-08-14 11:33:46 +0200

    fix(components): adding IconButton component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,22 +1,55 @@
-import { ReactNode, FC } from "react";
+import { ReactNode, FC, ButtonHTMLAttributes}  from "react";
+import SvgIcon, {SvgIconTypeMap} from "@mui/material/SvgIcon"
+import Colors from "tailwindcss/colors"
 
-interface ButtonProps {
-  color?: string;
-  children?: ReactNode;
-  onClick?: () => void;
+type SvgIconComponent = typeof SvgIcon;
+type TailwindColor = keyof typeof Colors;
+
+type IconColor = SvgIconTypeMap["props"]["color"];
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+  color?: TailwindColor,
+  children?: ReactNode
+};
+
+interface IconButtonProps extends Omit<ButtonProps, 'color'> {
+  icon: SvgIconComponent,
+  color?: IconColor,
 }
 
 export const Button: FC<ButtonProps> = ({
   color,
   children,
-  onClick,
+  className,
+  ...attr
 }) => {
+
+  const backgroundColor =  color ? "bg-" + color + "-500": "bg-slate-200";
+  const defaultClasseName = `${ backgroundColor } hover:brightness-110 hover:drop-shadow-xl text-black py-2 px-4 rounded`
+
   return (
     <button
-      className={`bg-${color}-300 hover:brightness-110 hover:drop-shadow-xl text-black py-2 px-4 rounded`}
-      onClick={onClick}
+      className={className ? defaultClasseName + " " + className : defaultClasseName}
+      {...attr}
     >
       {children}
     </button>
   );
 }
+
+export const IconButton: FC<IconButtonProps> = ({
+  icon: Icon,
+  color,
+  className,
+  ...attr
+}) => {
+
+  const hover = color !== "disabled" ? "hover:scale-110" : "";
+  const cursor = color !== "disabled" ? "cursor-pointer" : "cursor-auto";
+
+  return (
+    <Button color="transparent" className={`p-1 py-2 ${hover} ${cursor} ${className ? className : ""} `} {...attr}>
+      <Icon color={color} />
+    </Button>
+  );
+};
