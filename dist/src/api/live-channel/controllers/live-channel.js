9b39c61cc3cdb0de2dbb0ef3c158d402bfa0484d
mezz <mohamed.mazouz@epitech.eu>
2023-08-21 10:28:24 +0200

    chore(dist): sharing dist folder

---
+++ dist/src/api/live-channel/controllers/live-channel.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * live-channel controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::live-channel.live-channel');
