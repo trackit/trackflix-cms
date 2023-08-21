c54d4c5894150faa589afd17aff074b92916c850
mezz <mohamed.mazouz@epitech.eu>
2023-08-21 10:09:51 +0200

    live channel test page + strapi install

---
+++ src/api/live-channel/controllers/live-channel.ts
@@ -0,0 +1,7 @@
+/**
+ * live-channel controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::live-channel.live-channel');
---
+++ src/api/live-channel/services/live-channel.ts
@@ -0,0 +1,7 @@
+/**
+ * live-channel service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::live-channel.live-channel');
