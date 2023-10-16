cf9c0cd3a901f07e2615842816077f4c7a6c7a2f
maramtrackIT <maram@trackit.io>
2023-10-16 14:31:42 +0200

    fix/remove comments

--- src/plugins/docusaurus/admin/src/pages/App/index.tsx
+++ src/plugins/docusaurus/admin/src/pages/App/index.tsx
@@ -1,10 +1,3 @@
-/**
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- *
- */
-
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { AnErrorOccurred } from '@strapi/helper-plugin';
