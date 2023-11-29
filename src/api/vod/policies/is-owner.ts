1da920972cb4b0cc13d82f79713cd9e5d7fbe9f0
Mohamed MAZOUZ <mohamed.mazouz@epitech.eu>
2023-11-29 16:16:14 +0100

    feat(ownership): add owner control policie on delete/update

---
+++ src/api/vod/policies/is-owner.ts
@@ -0,0 +1,20 @@
+const { errors } = require('@strapi/utils');
+const { PolicyError } = errors;
+
+export default async (policyContext, config, { strapi }) => {
+    const { user } = policyContext.state;
+    const { id } = policyContext.params;
+
+    const vod = await strapi.entityService.findOne("api::vod.vod", id);
+
+    if (vod.owner) {
+        const ownerId = vod.owner;
+        const owner = await strapi.entityService.findOne("plugin::users-permissions.user", ownerId);
+        if (user.id != ownerId) {
+            throw new PolicyError(`You are not allowed to perform an action on this VOD. please contact his owner`,  {
+                "owner": owner.email
+            });
+        }
+    }
+    return true;
+}
