d1223883bc1b270fb7cb37385823c9d948f5d4af
Mohamed MAZOUZ <mohamed.mazouz@epitech.eu>
2023-11-30 04:06:32 +0100

    feat(s3): default plugin config + no credentials patch

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,16 +1,39 @@
-export default {
-    // ...
-    'cloudfront-metrics': {
-      enabled: true,
-      resolve: './src/plugins/cloudfront-metrics'
-    },
-    'custom-selects': {
-      enabled: true,
-      resolve: './src/plugins/custom-selects'
-    },
-    'docusaurus': {
-      enabled: true,
-      resolve: './src/plugins/docusaurus'
-    },
-    // ...
-  }
+import AWS from 'aws-sdk';
+
+
+export default async ({ env }) => {
+
+  return {
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            region: env('AWS_REGION'),
+            params: {
+              ACL: 'private', // <== set ACL to private
+              Bucket: env('AWS_BUCKET'),
+            },
+          },
+        },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},   
+          },
+        },
+      },
+      'cloudfront-metrics': {
+        enabled: true,
+        resolve: './src/plugins/cloudfront-metrics'
+      },
+      'custom-selects': {
+        enabled: true,
+        resolve: './src/plugins/custom-selects'
+      },
+      'docusaurus': {
+        enabled: true,
+        resolve: './src/plugins/docusaurus'
+      }
+}
+}
