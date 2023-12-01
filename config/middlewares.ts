810581ed129f2a290c3857a18b0e2d478ee81038
Mazouz <58136393+mezzLMC@users.noreply.github.com>
2023-12-01 15:17:02 +0100

    feat(Storage): S3 bucket instead of EC2 storage for images and thumbnails (#17)
    * feat(s3): default plugin config + no credentials patch
    ---------
    
    Co-authored-by: Nathan Agez <nathan.agez@gmail.com>

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,32 +1,45 @@
-export default [
-  'strapi::errors',
-  {
-    name: "strapi::security",
-    config: {
-    contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-        "connect-src": ["'self'", "https:"],
-        "img-src": ["'self'", "data:", "blob:", "*"],
-        "media-src": ["'self'", "data:", "blob:", "*"],
-        "default-src":["'self'"],
-        "script-src": ["'self'", "'unsafe-inline'"],
-        upgradeInsecureRequests: null,
+export default ({ env }) => (
+  [
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'market-assets.strapi.io',
+              env('AWS_BUCKET_ENDPOINT'),
+              
+            ],
+            'media-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'market-assets.strapi.io',
+              env('AWS_BUCKET_ENDPOINT'),
+            ],
+            upgradeInsecureRequests: null,
+          },
         },
+      },
     },
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    {
+      name: 'strapi::favicon',
+      config: {
+        path: './src/admin/extensions/trackit.png'
+      },
     },
-  },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  {
-    name: 'strapi::favicon',
-    config: {
-      path: './src/admin/extensions/trackit.png'
-    },
-  },
-  'strapi::public',
-];
+    'strapi::public',
+  ]
+);
