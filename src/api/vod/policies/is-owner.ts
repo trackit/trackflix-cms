0bb024d70eee9a9a88f0bd9fdd22af078a7c868f
Nathan Agez <nathan.agez@gmail.com>
2023-12-01 15:10:15 +0100

    refactor(role): edit role name

--- src/api/vod/policies/is-owner.ts
+++ src/api/vod/policies/is-owner.ts
@@ -7,14 +7,16 @@ export default async (policyContext, config, { strapi }) => {
 
     const vod = await strapi.entityService.findOne("api::vod.vod", id);
 
-    if (vod.owner) {
-        const ownerId = vod.owner;
-        const owner = await strapi.entityService.findOne("plugin::users-permissions.user", ownerId);
-        if (user.id != ownerId) {
-            throw new PolicyError(`You are not allowed to perform an action on this VOD. please contact his owner`,  {
-                "owner": owner.email
-            });
-        }
+    if (!vod.owner) {
+      throw new PolicyError('You are not allowed to perform an action on this VOD. please contact his owner');
+    }
+
+    const ownerId = vod.owner;
+    const owner = await strapi.entityService.findOne("plugin::users-permissions.user", ownerId);
+    if (user.id != ownerId) {
+      throw new PolicyError(`You are not allowed to perform an action on this VOD. please contact his owner`,  {
+        "owner": owner.email
+      });
     }
     return true;
 }
