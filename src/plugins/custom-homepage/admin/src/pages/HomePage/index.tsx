e64e51ebb4c110e724f6d8fdba3a9ef1da03f26e
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2023-12-11 15:40:18 +0100

    style(custom-ui): remove logs and fix linting errors

--- src/plugins/custom-homepage/admin/src/pages/HomePage/index.tsx
+++ src/plugins/custom-homepage/admin/src/pages/HomePage/index.tsx
@@ -1,44 +1,56 @@
-import { Box, Typography, Flex, Layout, Grid, GridItem, Button } from "@strapi/design-system"
-import { getFetchClient } from "@strapi/helper-plugin"
+import { Box, Typography, Flex, Layout, Grid, GridItem, Button } from '@strapi/design-system'
+import { useFetchClient } from '@strapi/helper-plugin'
 import React, { CSSProperties, useEffect, useState } from 'react';
-import pluginId from '../../pluginId';
-import { LiveChannel, Video } from "./interface";
+import { LiveChannel } from './interface';
 
 const vodThumbnailStyle : CSSProperties = {
   width: 300,
-  aspectRatio: "16/9"
+  aspectRatio: '16/9'
 }
 
 const liveThumbnailStyle: CSSProperties = {
   width: 280,
   height: 280,
-  overflow: "hidden",
-  borderRadius: "100%",
+  overflow: 'hidden',
+  borderRadius: '100%',
 }
 
 const cardTitleStyle: CSSProperties = {
-  textAlign: "center",
+  textAlign: 'center',
 }
 
-const navigateCollectionType = (collectionType: 'vod' | 'live') => {
-  const type = collectionType === 'vod' ? 'vod' : 'live-channel';
-  window.location.href = `${window.location.origin}/admin/content-manager/collectionType/api::${type}.${type}`;
+enum CollectionType {
+  Vod = 'vod',
+  Live = 'live-channel',
+}
+
+enum VODSortAttribute {
+  Latest = 'createdAt',
+  MostViewed = 'views',
+}
+
+const getUrl = (type: CollectionType, sortAttribute?: VODSortAttribute) =>
+  `/content-manager/collection-types/api::${type}.${type}?sort=${sortAttribute}:desc&_limit=1`;
+
+const navigateCollectionType = (type: CollectionType) => {
+  window.location.href = `${window.location.origin}/admin/${getUrl(type)}`;
 }
 
 const LastLiveChanel = () => {
-  const { get } : { get: (route: string) => Promise<any>} = getFetchClient()
+  const { get }  = useFetchClient()
   const [lastLive, setLastLive] = useState<LiveChannel | null>(null);
-  const [thumbnailUrl, setThumbnailUrl] = useState<string>("");
+  const [thumbnailUrl, setThumbnailUrl] = useState<string>('');
 
   useEffect(() => {
-      if (lastLive !== null) return;
-      (async () => {
-        const lastVideoResponse = await get(`/${pluginId}/live`);
-        if (!lastVideoResponse?.data?.results || lastVideoResponse?.data?.results?.length <= 0) return;
-        const live = lastVideoResponse.data.results[0] as LiveChannel;
-        setLastLive(live);
-      })()
-  }, [])
+    const fetchThumbnail = async () => {
+      const endpoint = getUrl(CollectionType.Live, VODSortAttribute.Latest);
+      const lastLive = await get(endpoint);
+      if (!lastLive?.data?.results || lastLive?.data?.results?.length <= 0) return;
+      setLastLive(lastLive.data.results[0] as LiveChannel);
+    }
+    if (lastLive === null)
+      fetchThumbnail();
+  });
 
   useEffect(() => {
     if (lastLive === null) return;
@@ -48,28 +60,26 @@ const LastLiveChanel = () => {
 
   return (
     <Box
-      as="aside"
-      aria-labelledby="join-the-community"
-      background="neutral0"
+      as='aside'
+      aria-labelledby='join-the-community'
+      background='neutral0'
       hasRadius
       paddingRight={5}
       paddingLeft={5}
       paddingTop={6}
       paddingBottom={6}
-      shadow="tableShadow"
+      shadow='tableShadow'
       height={500}
     >
-      <Flex direction="column" gap={8}>
-        <Typography as="h2" variant="alpha">Live channel</Typography>
-        <Box style={liveThumbnailStyle} shadow="tableShadow" background="neutral100">
+      <Flex direction='column' gap={8}>
+        <Typography as='h2' variant='alpha'>Live channel</Typography>
+        <Box style={liveThumbnailStyle} shadow='tableShadow' background='neutral100'>
           {thumbnailUrl &&
-            <img style={{overflow: "hidden", width: "100%"}} src={thumbnailUrl}/>
+            <img style={{overflow: 'hidden', width: '100%'}} src={thumbnailUrl}/>
           }
         </Box>
-        <Button onClick={() => {
-          navigateCollectionType('live');
-        }}>
-          <Typography as="h4" >Schedule a session</Typography>
+        <Button onClick={() => { navigateCollectionType(CollectionType.Live) }} >
+          <Typography as='h4'>Schedule a session</Typography>
         </Button>
       </Flex>
     </Box>
@@ -79,11 +89,11 @@ const LastLiveChanel = () => {
 const Separator = () => {
   return (
     <Box
-      as="hr"
+      as='hr'
       style={{
-        border: "none",
+        border: 'none',
         height: 1,
-        backgroundColor: "#e3e9f3",
+        backgroundColor: '#e3e9f3',
         marginTop: 10,
         marginBottom: 10,
       }}
@@ -91,8 +101,8 @@ const Separator = () => {
   );
 }
 
-const VODContainer = ({ variant }: { variant: 'latest' | 'most-viewed' }) => {
-  const { get } : { get: (route: string) => Promise<any>} = getFetchClient()
+const VODContainer = ({ sortAttribute }: { sortAttribute: VODSortAttribute }) => {
+  const { get } = useFetchClient()
   const [video, setVideo] = useState<{
     name: string;
     thumbnail: string;
@@ -100,73 +110,60 @@ const VODContainer = ({ variant }: { variant: 'latest' | 'most-viewed' }) => {
   } | null>(null);
 
   useEffect(() => {
-    if (video !== null) return
-    (async () => {
-      const lastVideoResponse = await get(`/${pluginId}/vod`)
+    const fetchVod = async () => {
+      const endpoint = getUrl(CollectionType.Vod, sortAttribute);
+      console.log(endpoint)
+      const lastVideoResponse = await get(endpoint);
       const { results } = lastVideoResponse.data;
-      if (!results || results.length <= 0) return;
+      console.log(sortAttribute, results);
+      if (!results || results?.length === 0) return;
 
-      const getVideo = () => {
-        switch (variant) {
-          case 'latest':
-            return results.reduce((prev: Video, current: Video) => {
-              if (prev.createdAt < current.createdAt) {
-                return current;
-              }
-              return prev;
-            }, results[0]);
-          case 'most-viewed':
-            return results.reduce((prev: Video, current: Video) => {
-              if (prev.views < current.views) {
-                return current;
-              }
-              return prev;
-            }, results[0]);
-          default:
-            return results[0];
-        }
-      }
-      const video = getVideo();
       setVideo({
-        name: video.Name,
-        thumbnail: video.Thumbnails[0].url,
-        views: video.views,
+        name: results[0].Name,
+        thumbnail: results[0].Thumbnails[0]?.url,
+        views: results[0].views,
       });
-    })()
-  }, [video]);
+    }
+    if (video === null)
+      fetchVod();
+  });
 
   return (
     <Box
-      as="aside"
-      aria-labelledby="join-the-community"
-      background="neutral0"
+      as='aside'
+      aria-labelledby='join-the-community'
+      background='neutral0'
       hasRadius
       paddingRight={5}
       paddingLeft={5}
       paddingTop={4}
       paddingBottom={6}
-      shadow="tableShadow"
-      style={{position: "relative", width: 350}}
+      shadow='tableShadow'
+      style={{ position: 'relative', width: 350 }}
     >
-      <Typography style={cardTitleStyle} as="h2" variant="alpha">{variant === 'latest' ? 'Latest VOD': 'Most viewed 🔥'}</Typography>
+      <Typography style={cardTitleStyle} as='h2' variant='alpha'>
+        {sortAttribute === VODSortAttribute.Latest ? 'Latest VOD': 'Most viewed 🔥'}
+      </Typography>
       <Separator />
-      <Flex direction="column" gap={8} style={{marginTop: 30}}>
-        <Flex direction="column" gap={1}>
-          <Typography as="h2" variant="alpha" style={{width: "100%", textAlign: "center"}}>{video ? video.name : ""}</Typography>
+      <Flex direction='column' gap={8} style={{marginTop: 30}}>
+        <Flex direction='column' gap={1}>
+          <Typography as='h2' variant='alpha' style={{width: '100%', textAlign: 'center'}}>
+            {video ? video.name : ''}
+          </Typography>
           <Typography>by Author</Typography>
-            {variant === 'most-viewed' &&
-            <Typography variant="beta">{video ? `${video.views} view(s)` : ""}</Typography>
-          }
+            {sortAttribute === VODSortAttribute.MostViewed &&
+              <Typography variant='beta'>{video ? `${video.views} view(s)` : ''}</Typography>
+            }
         </Flex>
-        <Box style={vodThumbnailStyle} shadow="tableShadeow" background="neutral100">
-          {video?.thumbnail != "" &&
-            <img style={{aspectRatio: "16/9", width: "100%"}} src={video?.thumbnail}/>
+        <Box style={vodThumbnailStyle} shadow='tableShadeow' background='neutral100'>
+          {video?.thumbnail != '' &&
+            <img style={{aspectRatio: '16/9', width: '100%'}} src={video?.thumbnail}/>
           }
         </Box>
-        <Button onClick={() => {
-          navigateCollectionType('vod');
-        }}>
-          <Typography as="h4" >Manage your VOD's</Typography>
+        <Button
+          onClick={() => { navigateCollectionType(CollectionType.Vod) }}
+        >
+          <Typography as='h4'>Manage your VOD's</Typography>
         </Button>
       </Flex>
     </Box>
@@ -180,9 +177,9 @@ const HomePage = () => {
           <Grid>
             <GridItem col={8} s={12}>
               <Box paddingLeft={6} paddingBottom={10}>
-                <Flex direction="column" alignItems="flex-start" gap={5}>
-                  <Typography as="h1" variant="alpha">Welcome to your Trackflix CMS 👋</Typography>
-                  <Typography as="p" variant="epsilon" width="70%">
+                <Flex direction='column' alignItems='flex-start' gap={5}>
+                  <Typography as='h1' variant='alpha'>Welcome to your Trackflix CMS 👋</Typography>
+                  <Typography as='p' variant='epsilon' width='70%'>
                     We hope you are making progress on your project! Feel free to read the latest news about Strapi.
                     We are giving our best to improve the product based on your feedback.
                   </Typography>
@@ -190,10 +187,10 @@ const HomePage = () => {
               </Box>
             </GridItem>
           </Grid>
-          <Flex direction="row" alignItems="stretch" justifyContent="center" gap={10}>
-            <VODContainer variant="latest" />
+          <Flex direction='row' alignItems='stretch' justifyContent='center' gap={10}>
+            <VODContainer sortAttribute={VODSortAttribute.Latest} />
             <LastLiveChanel />
-            <VODContainer variant="most-viewed" />
+            <VODContainer sortAttribute={VODSortAttribute.MostViewed} />
           </Flex>
       </Box>
     </Layout>
