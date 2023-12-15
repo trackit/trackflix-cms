bace4c54988aebec8acda6588ea04cc66ca58421
Mohamed MAZOUZ <mohamed.mazouz@epitech.eu>
2023-12-15 02:45:32 +0100

    feat(Uploading): change requests body max size

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -32,7 +32,17 @@ export default ({ env }) => (
     'strapi::poweredBy',
     'strapi::logger',
     'strapi::query',
-    'strapi::body',
+    {
+      name: "strapi::body",
+      config: {
+        formLimit: "256mb", // modify form body
+        jsonLimit: "256mb", // modify JSON body
+        textLimit: "256mb", // modify text body
+        formidable: {
+          maxFileSize: 10000 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+        },
+      },
+    },
     'strapi::session',
   'strapi::cors',
   'strapi::poweredBy',
