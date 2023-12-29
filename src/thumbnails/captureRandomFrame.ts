8496a6551d07f31a1bca4eaa053c9196b140fad6
Mohamed MAZOUZ <mohamed.mazouz@epitech.eu>
2023-12-29 17:13:50 +0100

    chore(thumbnails): remove useless comments

--- src/thumbnails/captureRandomFrame.ts
+++ src/thumbnails/captureRandomFrame.ts
@@ -2,9 +2,6 @@ import Ffmpeg from "fluent-ffmpeg";
 import libPath from "path"
 import {existsSync, mkdirSync} from "fs";
 
-
-//https://trackflix-thumbnail-storage-test.s3.us-west-2.amazonaws.com/AWS_Rekognition_and_Deep_Fashion2_The_Future_of_Fashion_Classification_4b293659dc.mp4
-
 const captureRandomFrame = (data : {media_url: string, name: string}) => {
   const {media_url, name} = data;
   const fileName = media_url.split("/").pop().split(".")[0];
